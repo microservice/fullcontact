10e683fac824c0d3959c04b5355698790792e64c
Steve Peak <steve@stevepeak.net>
2019-02-27 08:43:15 +0100

    jsonify the response to set correct headers

--- app.py
+++ app.py
@@ -3,16 +3,16 @@
 import os
 import sys
 import requests
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 
 app = Flask(__name__)
 
 
-def api(endpoint, query):
+def api(endpoint, data):
     res = requests.post(
         f'https://api.fullcontact.com/v3/{endpoint}.enrich',
-        data=query,
+        data=data,
         headers={
             'Authorization': f'Bearer {os.getenv("API_KEY")}',
             'User-Agent': os.getenv('USER_AGENT')
@@ -29,17 +29,17 @@ def api(endpoint, query):
         # TODO res.headers['X-Rate-Limit-Remaining']
         # TODO res.headers['X-Rate-Limit-Reset']
         # TODO res.headers['X-Rate-Limit']
-        return res.text
+        return res.json()
 
 
 @app.route('/person', methods=['POST'])
 def person():
-    return api('person', request.data)
+    return jsonify(api('person', request.data))
 
 
 @app.route('/company', methods=['POST'])
 def company():
-    return api('company', request.data)
+    return jsonify(api('company', request.data))
 
 
 if __name__ == '__main__':
